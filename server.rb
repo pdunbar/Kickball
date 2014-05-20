751a3346717a4fe34362f181798b0fc168a65b05
Peter Dunbar <Peter.joseph.dunbar@gmail.com>
2014-05-20 15:07:38 -0400

    CSV WORKING

--- server.rb
+++ server.rb
@@ -1,32 +1,24 @@
 require 'sinatra'
 require 'csv'
-
+require 'pry'
 
 get '/' do
   @rosters = {}
   @positions = {}
 
-  CSV.foreach(lackp_starting_rosters.csv, headers: true) do |row|
+  CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
       team = row["team"]
       position = row["position"]
       last_name = row["last_name"]
-      first_name = row["firs_name"]
+      first_name = row["first_name"]
 
-      @positions[position] = {team: [first_name + " " + last_name]}
-      @rosters[team] = {position: [first_name + " " + last_name]}
+
+      @positions[position] = {team => first_name}
+      @rosters[team] = {position => first_name}
     end
+
+    erb :teams
 end
 
 
-get '/rosters/:team' do
-  @rosters
-  erb :teams
-end
-
-
-get 'postions/:position' do
-  @position = params[:position]
-  # The :task_name is available in our params hash
-  erb :positions
-end
 
