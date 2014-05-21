33b93dafc1b7a07c7b76bc3028fd749413a7a4f2
Peter Dunbar <Peter.joseph.dunbar@gmail.com>
2014-05-20 21:17:29 -0400

    - Defined method 'load_list' to handle loading the rosters - in teams.rb, rosters are finally being pulled based on the team in the URL

--- server.rb
+++ server.rb
@@ -2,23 +2,30 @@ require 'sinatra'
 require 'csv'
 require 'pry'
 
-get '/' do
-  @rosters = {}
-  @positions = {}
-
+def load_list
+  rosters =[]
   CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
-      team = row["team"]
-      position = row["position"]
-      last_name = row["last_name"]
-      first_name = row["first_name"]
+    roster = {
+      first_name: row ["first_name"],
+      last_name: row["last_name"],
+      position: row["position"],
+      team: row["team"]
+    }
 
-
-      @positions[position] = {team => first_name}
-      @rosters[team] = {position => first_name}
-    end
-
-    erb :teams
+  rosters << roster
+  end
+  rosters
 end
 
+get'/' do
+  @rosters = load_list
+  erb :teams
+end
 
-
+get '/teams/:team' do
+  @rosters = load_list
+  @roster = @rosters.find do |roster|
+    roster[:team] == params[:team]
+  end
+  erb :teams
+end
